759f0ddd6562d3fc48c87cc46779d1941eb623e6
limingle <lml241@163.com>
2016-04-01 17:46:58 +0800

    俄罗斯方块修改

--- Unit06/06-001俄罗斯方块/app/index.js
+++ Unit06/06-001俄罗斯方块/app/index.js
@@ -2,7 +2,10 @@
  * Created by Administrator on 16-3-18.
  */
 init(50,"mylegend",320,480,main);
-
+var START_X1=15,
+    START_Y1=21,
+    START_X2=228,
+    START_Y2=65;
 var backLayer,
     loadingLayer,
     graphicsMap,
@@ -15,6 +18,33 @@ var imgData=new Array(
     {name:"r3",path:"images/r3.png"},
     {name:"r4",path:"images/r4.png"}
 )
+
+var map=[
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+];
+var nodeList=[];
+var bitmapDataList=[];
+var nextBox,nowBox;
+var BOX;
 
 function main(){
     backLayer=new LSprite();
@@ -30,6 +60,14 @@ function main(){
 
 function gameInit(result){
     imglist=result;
+
+    bitmapDataList=[
+        new LBitmapData(imglist["r1"]),
+        new LBitmapData(imglist["r2"]),
+        new LBitmapData(imglist["r3"]),
+        new LBitmapData(imglist["r4"])
+    ]
+
     backLayer.removeChild(loadingLayer);
     loadingLayer=null;
 
@@ -57,5 +95,55 @@ function gameToStart(){
     backLayer.removeAllChild();
     var bitmap=new LBitmap(new LBitmapData(imglist["backImage"]));
     backLayer.addChild(bitmap);
+    graphicsMap=new LSprite();
+    backLayer.addChild(graphicsMap);
+    nextLayer=new LSprite();
+    backLayer.addChild(nextLayer);
+    BOX=new Box();
+    initNodeList();
+    getNextBox();
+}
+
+function initNodeList(){
+    var i, j,nArr,bitmap;
+    for(i=0;i<map.length;i++){
+        nArr=[];
+        for(j=0;j<map[0].length;j++){
+            bitmap=new LBitmap(bitmapDataList[0]);
+            bitmap.x=bitmap.getWidth()*j+START_X1;
+            bitmap.y=bitmap.getWidth()*i+START_Y1;
+            graphicsMap.addChild(bitmap);
+            nArr[j]={
+                "index":-1,
+                "value":0,
+                "bitmap":bitmap
+            };
+        }
+        nodeList[i]=nArr;
+    }
+}
+
+function getNextBox(){
+    if(nextBox==null){
+        nextBox=BOX.getBox();
+    }
+    nowBox=nextBox;
+    nowBox.x=3;
+    nowBox.y=-4;
+    nextBox=BOX.getBox();
+
+    nextLayer.removeAllChild();
+    var i, j,bitmap;
+    for(i=0;i<nextBox.length;i++){
+        for(j=0;j<nextBox[0].length;j++){
+            if(nextBox[i][j]==0){
+                continue;
+            }
+            bitmap=new LBitmap(bitmapDataList[nextBox[i][j]-1]);
+            bitmap.x=bitmap.getWidth()*j+START_X2;
+            bitmap.y=bitmap.getWidth()*i+START_Y2;
+            nextLayer.addChild(bitmap);
+        }
+    }
 }
 
